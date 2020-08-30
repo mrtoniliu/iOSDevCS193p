f8dceec01bda0604eaff749eaf8818c4b096ee7f
Tony Liu <mrtoniliu@gmail.com>
2020-08-30 22:59:13 +0100

    Followed Lecture 1.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack() {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        // Passdown these functions to Views in ZStack
             .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View{
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack () {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill()
+            }
+        }
     }
 }
 
