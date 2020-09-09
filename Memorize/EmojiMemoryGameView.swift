2203e9b790b3a84cf341c2a5667f27ac671a10c4
Tony Liu <mrtoniliu@gmail.com>
2020-09-09 01:00:23 +0100

    Lecture 5: Implement custom shape: Pie

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -27,33 +27,42 @@ struct CardView: View{
     var card: MemoryGame<String>.Card
 
     var body: some View {
-        GeometryReader {geometry in
-            ZStack () {
-                if card.isFaceUp {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                    Text(card.content)
-                } else {
-                    if !card.isMatched{
-                        RoundedRectangle(cornerRadius: cornerRadius).fill()
-                    }
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ZStack () {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90),clockwise: true)
+                    .padding(5)
+                    .opacity(0.4)
+                Text(card.content)
+            } else {
+                if !card.isMatched{
+                    RoundedRectangle(cornerRadius: cornerRadius).fill()
                 }
             }
-            .font(Font.system(size: fontSize(for: geometry.size)))
         }
+        .font(Font.system(size: fontSize(for: size)))
     }
     
     // MARK: - Drawing Constants
     private let cornerRadius: CGFloat = 10
     private let edgeLineWidth: CGFloat = 3
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
     
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(gameModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(gameModel:game)
     }
 }
